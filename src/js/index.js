7646fc94f3f8d27e87589fa075c478fee0805257
Lola <yakjkf@gmail.com>
2020-07-28 12:48:00 +0600

    left menu script

--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,34 @@
     if(left_side) {
         const menu = document.querySelector('.menu');
         const links = menu.querySelectorAll('.menu-item');
-        links.forEach(link => link.addEventListener('click', e => left_side.classList.add('collapsed')))
+        links.forEach(link => link.addEventListener('click', e => {
+            e.stopPropagation();
+
+            const menuActiveItems = menu.querySelectorAll('.active.menu-item');
+            menuActiveItems.forEach(item => item.classList.remove('active'));
+
+            const menuType = link.dataset.menutype;
+            //console.log(menuType);
+            if (!left_side.classList.contains(menuType)) {
+                const classList = left_side.classList.value;
+                //str.match(/menu-\w*/g)
+                const menuClass = classList.match(/m-\w*/g);
+                console.log(menuClass);
+                if (menuClass) menuClass.forEach(cl => left_side.classList.remove(cl));
+
+                if (menuType) {
+                    left_side.classList.add(menuType);
+                }
+            }
+
+            link.classList.add('active');
+
+            const itemParent = link.parentNode;
+            if (itemParent && itemParent.classList.contains('submenu')) {
+                itemParent.closest('.menu-item').classList.add('active');
+            }
+        }));
     }
+
+
 })();
