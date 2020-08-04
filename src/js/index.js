84cfea95247df531c67ab3346bd33654e740721a
a_umut <askar.umut.kal@gmail.com>
2020-08-04 16:54:35 +0600

    modal show\hide functions

--- src/js/index.js
+++ src/js/index.js
@@ -55,4 +55,40 @@
         content_in_mt();
     });
 
+    //скрыть\показать модальное окно
+    const body = document.body;
+    const modal = document.querySelector('.modal');
+    const show_modal = document.querySelector('.show-modal');
+    const hide_modal = document.querySelector('.hide-modal');
+
+    //при клике на кнопку "show-modal", показать модальное окно
+    if(show_modal) {
+        show_modal.addEventListener('click', e => {
+            e.stopPropagation();
+            modal.classList.add('active');
+            body.classList.add("modal-bg");
+        });
+    }
+
+    //при клике на кнопку "hide-modal", скрыть модальное окно
+    if(hide_modal) {
+        hide_modal.addEventListener('click', e => {
+            e.stopPropagation();
+            modal.classList.remove('active');
+            body.classList.remove("modal-bg");
+        });
+    }
+
+    //скрыть модальное окно при клике вне блока модального окна
+    document.addEventListener('click', function(e) {
+        const target = e.target;
+        const its_modal = target == modal || modal.contains(target);
+        const its_btnSend = target == hide_modal;
+        const modal_is_active = modal.classList.contains('active');
+
+        if (!its_modal && !its_btnSend && modal_is_active) {
+            modal.classList.remove('active');
+            body.classList.remove("modal-bg");
+        }
+    });
 })();
