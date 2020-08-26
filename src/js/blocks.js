d53be63d50638c36d92dff2dc343a6afe8a13258
Bota <msbota@gmail.com>
2020-08-26 14:25:37 +0600

    .content.m-collapsed margin-left is corrected

---
+++ src/js/blocks.js
@@ -0,0 +1,17 @@
+
+(function () {
+    //const menuActiveItems = menu.querySelectorAll('.content-in');
+    // const c_in = document.getElementsByClassName('content-in');
+    // //c_in[0].style.backgroundColor = "black";
+    // const container_h = c_in[0].offsetHeight
+    // const container_h80 = (80*container_h)/100
+    //
+    // const c_center = document.getElementsByClassName('content__center');
+    // c_center[0].style.height = Math.round(container_h80)+"px"
+    //
+    // const c_item = document.querySelectorAll('.content-item__in');
+    // c_item.forEach(item => item.backgroundColor="black");
+    //
+    // console.log("---- container_h = "+ container_h);
+    // console.log("---- container_h80 = "+ container_h80);
+})();
