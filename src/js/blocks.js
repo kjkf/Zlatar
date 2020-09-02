ea67151ad60e152142c311ead29d17c58de389ca
Bota <msbota@gmail.com>
2020-09-02 16:41:58 +0600

    blocks.js is added, calculates height of blocks

--- src/js/blocks.js
+++ src/js/blocks.js
@@ -1,17 +1,60 @@
 
 (function () {
-    //const menuActiveItems = menu.querySelectorAll('.content-in');
-    // const c_in = document.getElementsByClassName('content-in');
-    // //c_in[0].style.backgroundColor = "black";
-    // const container_h = c_in[0].offsetHeight
-    // const container_h80 = (80*container_h)/100
-    //
-    // const c_center = document.getElementsByClassName('content__center');
-    // c_center[0].style.height = Math.round(container_h80)+"px"
-    //
-    // const c_item = document.querySelectorAll('.content-item__in');
-    // c_item.forEach(item => item.backgroundColor="black");
-    //
-    // console.log("---- container_h = "+ container_h);
-    // console.log("---- container_h80 = "+ container_h80);
+
+
+
+  function getStyle(elem, propertyName) {
+      return getComputedStyle(elem)[propertyName];
+  }
+
+  function setHeight_Content_In(){
+    var winowHeight = window.innerHeight;
+    var winowWidth = window.innerWidth;
+
+    var container = document.querySelector(".container");
+    var content_in = document.querySelector(".content-in");
+
+
+    var page_header = document.querySelector(".header");
+    var page_footer = document.querySelector(".footer");
+
+    var container_pt = getStyle(container, 'padding-top');
+    var container_pb = getStyle(container, 'padding-bottom');
+
+
+    var sum = parseInt(container_pt) + parseInt(container_pb) + page_header.clientHeight + page_footer.clientHeight
+    var content_in_h = winowHeight - sum;
+
+    console.log("page_header.clientHeight = " + page_header.clientHeight + "; page_footer.clientHeight = "+ page_footer.clientHeight);
+    console.log("container_pt = " + container_pt + "; container_pb = "+ container_pb);
+    console.log("sum = " + sum);
+    console.log("winowHeight = " + winowHeight);
+    console.log("content_in_h = " + content_in_h);
+
+    content_in.style.height = content_in_h + "px";
+
+    return content_in_h;
+  }
+  function setHeight_Content__Center(content_in_h){
+    var content__center = document.querySelector(".content__center");
+    var content__header = document.querySelector(".content__header");
+    var content__footer = document.querySelector(".content__footer");
+    //var content__breadcrumbs = document.querySelector(".content__breadcrumbs");
+
+    var sum = content__header.clientHeight + content__footer.clientHeight
+    var content__center_h = content_in_h - sum
+    console.log("content__center_h = " + content__center_h);
+    content__center.style.height = content_in_h + "px";
+  }
+  function calculateBlocksHeight(){
+    const content_in_h = setHeight_Content_In();
+    setHeight_Content__Center(content_in_h);
+  }
+
+  calculateBlocksHeight();
+
+  window.addEventListener("resize", calculateBlocksHeight);
+
+  
+
 })();
