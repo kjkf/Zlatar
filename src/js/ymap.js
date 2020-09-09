5c8cedc3f8ce7a7a85d387355ae6275a52f27aa8
Lola <yakjkf@gmail.com>
2020-09-09 17:01:29 +0600

    map bag fix

--- src/js/ymap.js
+++ src/js/ymap.js
@@ -1,6 +1,8 @@
 // Функция ymaps.ready() будет вызвана, когда
 // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
-ymaps.ready(init);
+const map = document.getElementById('map');
+if (map) ymaps.ready(init);
+
 function init(){
     // Создание карты.
     var myMap = new ymaps.Map("map", {
