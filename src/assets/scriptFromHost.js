a401af0b9bcb00877a27eb58033608db664cd9a8
Lola <yakjkf@gmail.com>
2020-11-18 15:32:05 +0600

    updates for host

--- src/assets/scriptFromHost.js
+++ src/assets/scriptFromHost.js
@@ -11551,27 +11551,29 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
         other_subblock.setAttribute("class", attrs)
 
     }
+    //collapse other descr block, for example: if you click to show activity descr block then
+    //management descr block will be collapsed
     function otherBlockCollapse(block, other_subblock_name){
-        const main_parent = block.closest(".compblock");
+        const main_parent = block.closest(".compblock")
         const other_subblock = main_parent.querySelector(".compblock-sub."+other_subblock_name);
-        const other_subblock_descr = other_subblock.querySelector(".compblock-sub__descr");
+        const other_subblock_descr = other_subblock.querySelector(".compblock-sub__descr")
 
         const attrs = other_subblock.getAttribute("class").replace("opened", "collapsed");
         other_subblock.setAttribute("class", attrs)
     }
+    //collapse descr block
     function collapseBlock(block ){
         const colapsedBlock = block.querySelector(".compblock-sub__descr");
         var attrs = block.getAttribute("class").replace("opened", "collapsed");
-
-
         if(block.getAttribute("class").includes("activity")){
             otherBlockOpen(block, "management");
         }else if (block.getAttribute("class").includes("management")) {
             otherBlockOpen(block, "activity");
         }
-        //alert("attrs = "+ attrs);
         block.setAttribute("class", attrs)
     };
+
+    //show descr block
     function showBlock(block){
         const colapsedBlock = block.querySelector(".compblock-sub__descr");
         var attrs = block.getAttribute("class").replace("collapsed", "opened");
@@ -11585,22 +11587,70 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
         block.setAttribute("class", attrs)
     };
 
+    //collapse all compblocks
+    function collapseAllCompblocks(){
+        const comblocks = document.querySelectorAll(".compblock");
+        //collapse all childs of comblock
+        for (var i=0; i< comblocks.length ; i++){
+            var comblock_activity = comblocks[i].querySelector(".compblock-sub.activity");
+            comblock_activity.setAttribute("class", "compblock-sub activity collapsed");
+            var comblock_manag = comblocks[i].querySelector(".compblock-sub.management");
+            comblock_manag.setAttribute("class", "compblock-sub management collapsed");
+        }
+    }
+    //adaptive calculation
+    function adaptiveView(){
+        const windowHeight = window.innerHeight;
+        const windowWidth = window.innerWidth;
+        const comblocks = document.querySelectorAll(".compblock");
+        //when screen width less or equal to 1024px
+        if (windowWidth <=1024){
+            //collapse 2d and 3d child of comblock
+            for (var i=1; i< comblocks.length ; i++){
+                var comblock_activity = comblocks[i].querySelector(".compblock-sub.activity");
+                comblock_activity.setAttribute("class", "compblock-sub activity collapsed");
+                var comblock_manag = comblocks[i].querySelector(".compblock-sub.management");
+                comblock_manag.setAttribute("class", "compblock-sub management collapsed");
+            }
+        }if (windowHeight<=700){
+            collapseAllCompblocks();
+        }
+    }
+    //========================================================================================
+    // click on Title
+    function clickTitleEvent(block, item, adaptive){
+        if(block.getAttribute("class").includes("opened")){
+            if (!adaptive){
+                collapseBlock(block)
+            } else {
+                collapseAllCompblocks()
+            }
+        }else if (block.getAttribute("class").includes("collapsed")){
+            if(adaptive)collapseAllCompblocks();
+            showBlock(block);
+        }
+    }
+
     setTimeout(() => {
         const title = document.querySelectorAll(".compblock .title");
-
+        const windowWidth = window.innerWidth;
         if(title){
             title.forEach(item => {
                 item.addEventListener('click', event => {
-
+                    //if screen width is 1024px then only one comblock can be
+                    //opened and other will be collapsed totally
                     const  block = item.closest(".compblock-sub");
-                    if(block.getAttribute("class").includes("opened")){
-                        collapseBlock(block);
-                    }else if (block.getAttribute("class").includes("collapsed")){
-                        showBlock(block);
+                    if (windowWidth <=1024){
+                        clickTitleEvent(block, item, true);
+                    }else{
+                        clickTitleEvent(block, item, false);
                     }
                 });
             });
         }
+        //adaptive calculation
+        adaptiveView();
+//========================================================================================
     }, 500);
 
 })();
@@ -11650,7 +11700,6 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
 
         [nameInput, phoneInput].forEach(input => {
             input.addEventListener('blur', e => {
-
                 if (nameInput.value !== '' && phoneInput.value !== '') {
                     calculateBlock.classList.remove('disable');
                     calculateInputs.forEach(input => {
@@ -11775,12 +11824,10 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
 
     function validatePhone(email) {
         var re = /^(\+)?(\(\d{2,3}\) ?\d|\d)(([ \-]?\d)|( ?\(\d{2,3}\) ?)){5,12}\d$/;
-        console.log(re.test(String(email).toLowerCase()));
         return re.test(String(email).toLowerCase());
     }
 
     function isValidField(field) {
-        console.log(field);
         var isValid = true;
         if (!field || !field.value) {
             isValid = false;
@@ -11795,7 +11842,7 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
         } else {
             field.classList.remove('field-err');
         }
-        console.log('isValid = ', isValid);
+
         return isValid;
 
     }
@@ -11985,11 +12032,11 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
 
             if (hide_modal) {
                 hide_modal.addEventListener('click', function (e) {
-                    console.log('hide_modal CLICK');
+
                     e.stopPropagation();
 
                     var isValide = isFormValid(inputs);
-                    console.log('isValide = ' + isValide);
+
                     if (isValide) {
                         form.classList.remove('form-err');
                         var submitInput = modal.querySelector('input.submit-btn');
@@ -12041,6 +12088,7 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
                         submitInput.click();
                         modal.classList.remove('active');
                         body.classList.remove("modal-bg");
+                        prepareCalculateModals();
                     } else {
                         modal.classList.remove('active');
                         body.classList.remove("modal-bg");
@@ -12058,6 +12106,7 @@ function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "functi
         prepareModals();
         prepareCatalogPhoneForm();
         prepareCalculateModals();
+
     }, 500);
 
 })();
